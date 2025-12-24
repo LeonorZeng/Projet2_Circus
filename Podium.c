588ec911fa5d807e1f6b3d545ea65f1728c869e0
Ismail-Fourra <ismailfourra@gmail.com>
2025-12-24 12:54:47 +0100

    typedef Podium en Vecteur et commencer à adapter les fonction en conséquence

--- Podium.c
+++ Podium.c
@@ -2,36 +2,23 @@
 #include <stdlib.h>
 
 int podium_init(Podium* p, int cap) {
-    if (cap <= 0) return 0;
-
-    p->animaux = (int*)malloc((size_t)cap * sizeof(int));
-    if (p->animaux == NULL) return 0;
-
-    p->taille = 0;
-    p->cap = cap;
-    return 1;
+    return initVecteur(p, cap);
 }
 
 void podium_free(Podium* p) {
-    if (!p) return;
-    free(p->animaux);
-    p->animaux = NULL;
-    p->taille = 0;
-    p->cap = 0;
+    detruireVecteur(p);
 }
 
 int podium_push(Podium* p, int animal) {
-    if (p->taille >= p->cap) return 0;     // pile pleine
-    p->animaux[p->taille] = animal;        // on met au sommet
-    p->taille++;                           // sommet monte
-    return 1;
+    return ajouter(p, animal);
 }
 
-int podium_pop(Podium* p, int* animal) {
-    if (p->taille <= 0) return 0;          // pile vide
-    p->taille--;                           // on descend le sommet
-    *animal = p->animaux[p->taille];       // on r�cup�re l'ancien sommet
-    return 1;
+int podium_pop(Podium* p) {
+    int animal;
+    assert(p->nbElements >= 0);
+    animal = obtenir(p, p->nbElements);
+    supprimer(p, p->nbElements);
+    return animal;
 }
 
 void podium_print(const Podium* p) {
--- Podium.h
+++ Podium.h
@@ -1,11 +1,7 @@
 ﻿#pragma once
-#include <stdio.h>
+#include "vecteur.h"
 
-typedef struct {
-    int* animaux;  // tableau dynamique (cap éléments)
-    int taille;    // nb d'animaux dans la pile
-    int cap;       // capacité max d'animaux
-} Podium;
+typedef Vecteur Podium;
 
 // Initialise une pile vide de capacité cap
 int podium_init(Podium* p, int cap);
@@ -16,7 +12,7 @@ void podium_free(Podium* p);
 // Empile (push) un animal. Renvoie 1 si OK, 0 si pile pleine.
 int podium_push(Podium* p, int animal);
 
-// D�pile (pop) le sommet. Met la valeur dans *animal. Renvoie 1 si OK, 0 si vide.
+// Dépile (pop) le sommet. Met la valeur dans *animal. Renvoie 1 si OK, 0 si vide.
 int podium_pop(Podium* p, int* animal);
 
 // Affichage debug (bas -> haut) en nombres
@@ -24,4 +20,3 @@ void podium_print(const Podium* p);
 
 // Rotation bas -> haut (utile pour NI/MA). Renvoie 1 si OK, 0 si <= 1 �l�ment.
 int podium_bas_vers_haut(Podium* p);
-#pragma once
